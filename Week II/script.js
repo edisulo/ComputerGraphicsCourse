6c53017141a005180de8193406656e037b6d430f
edizzlla <edi.sulo01@gmail.com>
2024-10-08 12:40:31 +0200

    modified week 2

--- Week II/script.js
+++ Week II/script.js
@@ -10,7 +10,42 @@ const material = new THREE.MeshBasicMaterial({
 
 const box = new THREE.Mesh(geometry,material);
 
+scene.add(box);
+
+function randomInt(min,max){
+    return Math.random() * (max-min) + min;
+}
+
+for (var i = 0;i<1000;i++){
+    const sphereGeometry = new THREE.SphereGeometry(0.01,32,32);
+    const sphereMaterial = new THREE.MeshBasicMaterial({
+        color:0x136523,
+        wireframe:false
+    });
+
+const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+sphere.position.set(randomInt(-10,10),randomInt(-5,5),randomInt(-5,5));
+scene.add(sphere);
+}
 
 
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height);
+camera.position.z = 3;
+//camera.position.x = 1;
+scene.add(camera);
+
+box.rotation.x = Math.PI/4;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(800,600)
+document.getElementById('scene').appendChild(renderer.domElement);
+
+
+renderer.render(scene,camera);
 
 
