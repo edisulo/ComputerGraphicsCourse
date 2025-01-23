53f6ab79fd2b6db2066a0cb930dbb943e0d69eb3
edizzlla <edi.sulo01@gmail.com>
2025-01-23 18:07:08 +0100

    added chair, desk, whiteboard ...

---
+++ Assignment II/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        open: true, // Automatically open the app in your browser
+        port: 3000, // Set your development server port
+    },
+});
