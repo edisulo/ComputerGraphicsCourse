e30fbea33d0981ce908244fd4d4fa8bc864fab1e
edizzlla <edi.sulo01@gmail.com>
2025-01-23 21:39:56 +0100

    build problems

--- Assignment II/vite.config.js
+++ Assignment II/vite.config.js
@@ -5,4 +5,7 @@ export default defineConfig({
         open: true, // Automatically open the app in your browser
         port: 3000, // Set your development server port
     },
+    build: {
+                assetsDir: 'assets', // Directory for assets
+      }
 });
