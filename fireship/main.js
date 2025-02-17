2aa48fe2f6b045b6097bd32e3bc03c0afb98a7c6
edizzlla <edi.sulo01@gmail.com>
2025-02-17 18:58:25 +0100

    changed directory structure to allow for deployment

--- fireship/main.js
+++ fireship/main.js
@@ -1,65 +1,85 @@
-import'./style.css'
-import * as THREE   from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-
-
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-
+  canvas: document.querySelector("#bg"),
 });
+
 renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-// renderer.render(scene,camera);
-
-const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshStandardMaterial({color:0xFF6347});
-const torus = new THREE.Mesh(geometry,material);
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 torus.receiveShadow = true;
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(1,1,1);
+pointLight.position.set(1, 1, 1);
 pointLight.castShadow = true;
 scene.add(pointLight);
 
-// const ambientLight = new THREE.AmbientLight(0xffffff);
-// scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200,5);
-scene.add(lightHelper,gridHelper);
+const gridHelper = new THREE.GridHelper(200, 5);
+scene.add(lightHelper, gridHelper);
 
-const controls = new OrbitControls(camera,renderer.domElement);
 
-function addStar(){
-    const geometry = new THREE.SphereGeometry(0.25,24,24);
-    const material = new THREE.MeshStandardMaterial({color:0xffffff})
-    const star = new THREE.Mesh(geometry,material);
+const controls = new OrbitControls(camera, renderer.domElement); // -> OrbitControls is added 
 
-    const [x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100));
-    star.position.set(x,y,z);
-    scene.add(star)
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
 }
 
-Array(200).fill().forEach(addStar)
+Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('/pics/nasa.jpg')
+const spaceTexture = new THREE.TextureLoader().load("pics/nasa.jpg");
 scene.background = spaceTexture;
 
-function animate(){
-    requestAnimationFrame(animate);
-    torus.rotation.x+=0.01;
-    torus.rotation.y+=0.005;
-    torus.rotation.z+=0.01;
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("textures/dices.avif");
+const boxGeometry = new THREE.BoxGeometry(1,1,1);
 
-    controls.update();
-    renderer.render(scene,camera);
+
+const uvs = [
+0.5,0.5,0.75,0.5,0.5,0.75,0.75,0.75,
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+];
+
+boxGeometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs,2));
+
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
