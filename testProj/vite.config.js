d05de12bc77f60059840b83fc446384413d2d396
edizzlla <edi.sulo01@gmail.com>
2025-02-17 19:26:57 +0100

    added three as an optimized dependency

--- testProj/vite.config.js
+++ testProj/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
         open: true, // Automatically open the app in your browser
         port: 3000, // Set your development server port
     },
+    optimizeDeps: {
+      include: ["three"],
+    },
     base: './', // Ensures relative paths for assets
     build: {
       outDir: 'dist', // Output folder
